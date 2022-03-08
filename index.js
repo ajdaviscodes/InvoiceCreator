3ef5692b3b8cfe7e1dad76382eaf5882af1a55fb
[AJ Davis] <78609746+ajdaviscodes@users.noreply.github.com>
2022-03-08 16:00:41 -0600

    some javascript
    need to figure out better access to 'jobs' object to add total price in totalEl

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-const invoice = document.querySelector('.invoice')
+
+const btnEl1 = document.getElementById("button-1");
+const btnEl2 = document.getElementById("button-2");
+const btnEl3 = document.getElementById("button-3");
+const taskUl = document.getElementById("task-ul");
+const priceUl = document.getElementById("price-ul");
+const invoiceBtn = document.getElementById("invoice-btn");
+const deleteBtn = document.getElementById("delete");
+const totalEl = document.getElementById("total-el");
+
+let jobs = [
+    { task: "Wash Car", price: 10 },
+    { task: "Mow Lawn", price: 20 },
+    { task: "Pull Weeds", price: 30 },
+    
+  ];
+
+btnEl1.addEventListener("click", () => {
+    taskUl.innerHTML += `<li>${jobs[0].task}</li>`
+    priceUl.innerHTML += `<li><span>$</span>${jobs[0].price}</li>`
+    
+})
+
+btnEl2.addEventListener("click", () => {
+    taskUl.innerHTML += `<li>${jobs[1].task}</li>`
+    priceUl.innerHTML += `<li><span>$</span>${jobs[1].price}</li>`
+    
+})
+
+btnEl3.addEventListener("click", () => {
+    taskUl.innerHTML += `<li>${jobs[2].task}</li>`
+    priceUl.innerHTML += `<li><span>$</span>${jobs[2].price}</li>`
+    
+})
+
+
+
+
+
+
+
